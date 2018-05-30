041084d292303fae9f8e9fccfc313a27b50d3447
andycloke <apcloke@gmail.com>
2018-05-30 20:49:50 +0100

    Fix typo in comment

--- features/players/components/PlayerCarousel.tsx
+++ features/players/components/PlayerCarousel.tsx
@@ -4,7 +4,7 @@ import ImageCarousel from '../../common/components/ImageCarousel';
 import PLAYER_IMAGES from '../data/playerImages';
 
 /* For now this component is so simple it's almost redudant, since it just imports the data.
-However in a real world application it might handee fetching the data from a server etc. */
+However in a real world application it might handle fetching the data from the server etc. */
 
 const PlayersCarousel = () => <ImageCarousel images={PLAYER_IMAGES} />;
 
