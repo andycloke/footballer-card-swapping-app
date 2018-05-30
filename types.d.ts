9aaa4f4fd5bd19293ac78f1a314d9e61eca1584a
andycloke <apcloke@gmail.com>
2018-05-30 20:02:27 +0100

    Add types for react-native-swiper-flatlist library

--- types.d.ts
+++ types.d.ts
@@ -1,3 +1,20 @@
 declare module 'react-native-swiper-flatlist' {
-
+  type SwiperFlatListProps = {
+    data?: Array<any>;
+    children?: JSX.Element[];
+    renderItem?: (props: any) => React.Component;
+    onMomentumScrollEnd?: (e: any) => {};
+    vertical?: boolean;
+    index?: number;
+    renderAll?: boolean;
+    showPagination?: boolean;
+    paginationDefaultColor?: string;
+    paginationActiveColor?: string;
+    PaginationComponent?: React.Component;
+    autoplay?: boolean;
+    autoplayDelay?: number;
+    autoplayLoop?: boolean;
+  };
+  class SwiperFlatList extends React.Component<SwiperFlatListProps, any> {}
+  export default SwiperFlatList;
 }
