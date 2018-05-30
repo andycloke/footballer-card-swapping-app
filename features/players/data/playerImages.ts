c76c5411e5950ac86043009698cce655aab6a452
andycloke <apcloke@gmail.com>
2018-05-30 20:12:55 +0100

    Add player image assets

---
+++ features/players/data/playerImages.ts
@@ -0,0 +1,5 @@
+const player1 = require('../../../assets/images/1.png');
+const player2 = require('../../../assets/images/2.png');
+const player3 = require('../../../assets/images/3.png');
+
+export default [player1, player2, player3];
