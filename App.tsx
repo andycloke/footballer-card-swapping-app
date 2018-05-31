52cb4b7177708aa845946a2a482b2882efa5174b
andycloke <apcloke@gmail.com>
2018-05-31 07:33:07 +0100

    Add status bar with white text. Use RN component as app.json didnt work

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { StyleSheet, Text, View, Dimensions, Image } from 'react-native';
+import { StyleSheet, View, StatusBar } from 'react-native';
 import SwiperFlatList from 'react-native-swiper-flatlist';
+import { Constants } from 'expo';
 
 import { colors } from './styles';
 import PlayerMenu from './features/players/components/PlayerMenu';
@@ -9,18 +10,17 @@ export default class App extends React.Component<{}> {
   render() {
     return (
       <View style={styles.container}>
+        <StatusBar barStyle="light-content" />
         <PlayerMenu />
       </View>
     );
   }
 }
 
-export const { width, height } = Dimensions.get('window');
-
 const styles = StyleSheet.create({
   container: {
     backgroundColor: colors.bunting,
-    paddingTop: 30,
+    paddingTop: Constants.statusBarHeight,
     flex: 1
   }
 });
